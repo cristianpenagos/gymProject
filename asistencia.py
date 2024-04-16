326c2b22b6029f9facfb7e05c702d046c9e19d76
PenagozDev <cristianpenagoso01@gmail.com>
2024-04-16 13:07:34 -0500

    Terminado el metodo consultarIDUsuario

--- asistencia.py
+++ asistencia.py
@@ -2,20 +2,35 @@ from conexion import conectar
 
 import datetime
 
-fecha_actual = datetime.datetime.now()
+
 
 print(fecha_actual)
 
 
 
 
-def save_asistencia():
+def save_asistencia(qr):
     try: 
         db_connection = conectar()
         cursor = db_connection.cursor()
 
+        fecha_actual = datetime.datetime.now()
+        idUsuario = consultarIDUsuario(qr)
+
         query = "INSERT INTO asistencia (fecha, qrUsuario, usuario_idusuario) VALUES(%s, %s, %s)"
-        values = ()
+        values = (fecha_actual, qr, idUsuario)
+
+        cursor.execute(query, values)
+        db_connection.commit()
+        cursor.close()
+        db_connection.close()
+        return 'Asistencua guardada exitosamente'
+    except Exception as e:
+        return f'Error al guardar asistencia: {e}'
+    ###
+    
+
+
 
 
 
